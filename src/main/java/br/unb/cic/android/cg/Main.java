4ba3e6b31ed3832aaf34847fe81a51d6e24ffc58
fhandrick <handrick@gmail.com>
2022-01-11 16:26:37 +0100

    method that verifies if a given SootMethod is an Android sensitive method

--- src/main/java/br/unb/cic/android/cg/Main.java
+++ src/main/java/br/unb/cic/android/cg/Main.java
@@ -1,7 +1,8 @@
-package br.unb.cic.android.cg;
-
 import com.google.common.graph.GraphBuilder;
 import com.google.common.graph.MutableGraph;
+import com.google.common.graph.Traverser;
+
+import soot.G;
 import soot.Scene;
 import soot.jimple.infoflow.cmd.MainClass;
 import soot.jimple.toolkits.callgraph.CallGraph;
@@ -9,14 +10,25 @@ import soot.jimple.toolkits.callgraph.Edge;
 
 import soot.SootMethod;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.*;
+
+
 
 public class Main {
 
+
+	public static ArrayList<Node> sensitiveMethodsTotal = new ArrayList<Node>();
+	
     private static final String ERROR_MESSAGE =
             "Unexpected command line arguments. Please try: \n" +
             "java -jar cg.jar -a <APK_FILE> -p <ANDROID_SDK_PLATFORM> -s <FILE_SOURCE_SINKS>";
@@ -37,6 +49,11 @@ public class Main {
     }
 
     public static void main(String args[]) throws Exception {
+    	
+    	
+    	Path path = Path.of("sensitive-methods.txt");
+    	List<String> sensitiveMethods = Files.readAllLines(path);
+
         MainClass.main(args);
         if(!Scene.v().hasCallGraph()) {
             Logger.getLogger(MainClass.class.getName()).log(Level.SEVERE, "Failed while building " +
@@ -55,32 +72,112 @@ public class Main {
         while(iterator.hasNext()) {
             Edge e = iterator.next();
 
-            Node src = new Node(e.src(), computeMethodType(e.src()));
-            Node tgt = new Node(e.tgt(), computeMethodType(e.tgt()));
+            Node src = new Node(e.src(), computeMethodType(e.src(), sensitiveMethods));
+            Node tgt = new Node(e.tgt(), computeMethodType(e.tgt(), sensitiveMethods));
 
             g.putEdge(src, tgt);
+            
         }
+        
+        //List<Node> sensitiveMethodsDistinct = sensitiveMethodsTotal.stream().distinct().collect(Collectors.toList());
+        
+        
         Logger.getLogger(MainClass.class.getName()).log(Level.INFO," done. \n" +
                 " (*) Number of nodes in the CG: " + g.nodes().size() + "\n" +
-                " (*) Number of edges in the CG: " + g.edges().size()
+                " (*) Number of edges in the CG: " + g.edges().size() + "\n"
         );
-        // TODO: so, here we can traverse the graph `g` and
-        //   compute the distances.
+        
+        
+        ArrayList<Node> listEntryPoint = new ArrayList<Node>();
+        ArrayList<Node> listSensitiveMethod = new ArrayList<Node>();
+        
+        Set<Node> myNodes = g.nodes();
+        Iterator<Node> it = myNodes.iterator();
+        while(it.hasNext()){
+        	
+        	Node n = it.next();
+        	
+        	if (n.type.toString().equals("EntryPointMethod")) {
+        		listEntryPoint.add(n);
+        		//System.out.println((n.method).toString()+"----"+n.type);
+        	}
+        		
+        	if (n.type.toString().equals("SensitiveMethod")) {
+        		listSensitiveMethod.add(n);
+        		//System.out.println(n.method+"==>This is Sensitive: "+n.method.getDeclaringClass().getName()+"====="+n.method.getSubSignature());
+        	}
+        }
+        
+        //Print size lists
+        System.out.println(listEntryPoint.size());
+        System.out.println(listSensitiveMethod.size());
+        
+        
+        //Here we will run lists and sum the distance from all components.
+        int dtc = 0;
+        for (int i=0 ; i<listEntryPoint.size() ; i++) {
+        	for (int j=0 ; j<listSensitiveMethod.size() ; j++) {
+        		Node src = listEntryPoint.get(i);
+        		Node tgt = listSensitiveMethod.get(j);
+        		//sum all the distance calling distance method
+        		dtc = dtc + distance(src,tgt);
+        	}
+        }
+        //Print the distance
+        System.out.println(dtc);
+        
+        //here I check who is the adjacent Node of the first EntryPoint at the List
+        
+        Set<Node> checkAdjacent = g.adjacentNodes(listEntryPoint.get(0));
+        
+        Iterator<Node> ite = checkAdjacent.iterator();
+        while(ite.hasNext()){
+        	Node ajdNode = ite.next();
+        	System.out.println(("adjacent---"+ajdNode.method).toString()+"----"+ajdNode.type);
+        	
+        }
+          
+        //Here I print all the nodes traversed from the first element of the Entry Point.
+        Traverser.forGraph(g).breadthFirst(listEntryPoint.get(0))
+        .forEach(x->System.out.println(x.method));
+        
     }
+;        
+     
+        
+    
+    
+	static int distance(Node from, Node to) {
+	
+		return 1;
+	}
+	
 
-    static MethodType computeMethodType(SootMethod method) {
+    static MethodType computeMethodType(SootMethod method,List<String> lsensitive) throws IOException {
         if(Scene.v().getCallGraph().isEntryMethod(method)) {
             return MethodType.EntryPointMethod;
         }
-        else if(sensitiveMethod(method)) {
+        else if(sensitiveMethod(method,lsensitive)) {
             return MethodType.SensitiveMethod;
         }
         return MethodType.SimpleMethod;
     }
-
-    private static boolean sensitiveMethod(SootMethod method) {
-        return false; // TODO: Handrick, rewrite this method to
-                      //   check if `method` is a sensitive method.
+    //Method that check if a method is sensitive
+    //check if the method belongs to existent list of sensitive methods 
+    private static boolean sensitiveMethod(SootMethod method, List<String> lsensitive) throws IOException {
+    	
+       	int count = 0;
+       	boolean sensitive = false;
+    	while (lsensitive.size() > count) {
+    		if (lsensitive.get(count).contains(method.getDeclaringClass().getName()) && lsensitive.get(count).contains(method.getSubSignature())) {
+    			Node n = new Node(method,MethodType.SensitiveMethod );
+    			//Insert method into the list of sensitive methods 
+    			//sensitiveMethodsTotal.add(n);
+    			
+    			sensitive = true;
+    		}
+    		count++;
+    	}
+        return sensitive;   
     }
-
 }
